a560e294d6089a4f4846442e4a75f12b90e5f9f9
xinyan140@gmail.com <994321092@qq.com>
2016-07-25 13:56:55 +0800

    add env

--- lib/xinyan_gems.rb
+++ lib/xinyan_gems.rb
@@ -1,8 +1,14 @@
 require "xinyan_gems/version"
+require "dotenv"
+Dotenv.load
 
 module XinyanGems
   # Your code goes here...
   def self.hello
     p "hello to xinyan's gems"
+    db_uid = ENV['DB_UID']
+    db_seckey = ENV['DB_SECKEY']
+    p db_uid
+    p db_seckey
   end
 end
--- spec/xinyan_gems_spec.rb
+++ spec/xinyan_gems_spec.rb
@@ -1,4 +1,6 @@
 require 'spec_helper'
+require 'dotenv'
+Dotenv.load
 
 describe XinyanGems do
   it 'has a version number' do
@@ -7,5 +9,14 @@ describe XinyanGems do
 
   it 'does something useful' do
     expect(false).to eq(true)
+  end
+
+  it 'show dotenv variable' do
+    db_uid = ENV['DB_UID']
+    db_seckey = ENV['DB_SECKEY']
+    p db_uid
+    p db_seckey
+    expect(db_uid).not_to be nil
+    expect(db_seckey).not_to be nil
   end
 end
